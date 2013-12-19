12b80b43569d54f8d6bcc02c57553f81c9d495fd
g-van-vreckem <gvanvreckem@gmail.com>
2013-12-19 13:48:42 +0100

    cleaned dreamweaver polution

--- html/fr/index.php
+++ html/fr/index.php
@@ -95,11 +95,11 @@
                     <p><a class="btn btn-jmei" href="<?php echo Site::relativeUrl('contact');?>">Contactez-nous! &raquo;</a></p>
                 </div>
                 <div class="span4">
-                    <h2 style="text-align: left">Joyeuses fêtes!</h2>
+                    <h2>Joyeuses fêtes!</h2>
                     <a href="<?php echo Site::relativeUrl('realisations');?>" style="text-align: left"><img src="../img/home/month-radio.jpg?v=1" alt="Radiocommande Industrielle"></a>
                </div>
                 <div class="span4">
-                  <h2 style="text-align: left">Nos points forts</h2>
+                  <h2>Nos points forts</h2>
                   <ul>
                     <li>Personnalisation</li>
                     <li>Flexibilité</li>
