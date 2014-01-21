7c15e97f2aad436b5948f7aec5db3a9b4aabee8c
JMei-Manu <manu.piraprez@jmei.be>
2014-01-21 12:40:34 +0100

    Radio of the month january 2014

--- html/en/realisations.php
+++ html/en/realisations.php
@@ -13,6 +13,7 @@
 					<div>
 						<ul id="leftnav" class="nav nav-tabs nav-stacked">
 							<li class="nav-header">Featured Customization</li>
+                         <li class="active"><a href="#blog-2014-01">January 2014 <i class="icon-chevron-right pull-right"></i></a></li>
 							<li class="active"><a href="#2013-12">December 2013 <i class="icon-chevron-right pull-right"></i></a></li>
 							<li><a href="#blog-2013-11">November 2013 <i class="icon-chevron-right pull-right"></i></a></li>
 						</ul>
@@ -20,7 +21,14 @@
 				</div>
 				<div class="span9">
 				<!--Body content-->
-					<section id="blog-2013-12">
+                		<section id="blog-2014-01">
+						<h3>Protective headband (SAV)</h3>
+					  <img class="img-polaroid blog" src="../img/custom-product/2014-01-3000.jpg" alt="RCB3000 personnalisée avec retour d'informations">
+						<p>New protective headband, flexible and robust at the same time, developed by JMei. <br>
+						  In case of deterioration of the issuer RCB3000, this solution allows a fast and efficient troubleshooting without having to change the entire face of your RCB3000 model.</p>
+					</section>
+					<hr>            
+				  <section id="blog-2013-12">
 						<h3>RCB3000 bidirectional</h3>
 						<img class="img-polaroid blog" src="../img/custom-product/2013-12-3000.jpg" alt="Custom RCB3000 December 2013">
 						<p>RCB3000 tailored according the  customer's specifications.<br>
