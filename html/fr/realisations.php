10c2819cdc0e2814d89c5a202d3c58700ac7b95d
JMei-Manu <manu.piraprez@jmei.be>
2014-01-21 12:30:30 +0100

    Custom product fr: janvier 2014

--- html/fr/realisations.php
+++ html/fr/realisations.php
@@ -13,6 +13,7 @@
 					<div>
 						<ul id="leftnav" class="nav nav-tabs nav-stacked">
 							<li class="nav-header">Radiocommandes personnalisées</li>
+                            <li class="active"><a href="#blog-2014-01">Janvier 2014 <i class="icon-chevron-right pull-right"></i></a></li>
 							<li class="active"><a href="#blog-2013-12">Décembre 2013 <i class="icon-chevron-right pull-right"></i></a></li>
 							<li><a href="#blog-2013-11">Novembre 2013 <i class="icon-chevron-right pull-right"></i></a></li>
 						</ul>
@@ -20,6 +21,14 @@
 				</div>
 				<div class="span9">
 				<!--Body content-->
+					<section id="blog-2014-01">
+						<h3>Arceau de protection (SAV)</h3>
+					  <img class="img-polaroid blog" src="../img/custom-product/2014-01-3000.jpg" alt="RCB3000 personnalisée avec retour d'informations">
+						<p>Nouvel arceau, souple et robuste à la fois, développé par JMei.<br>
+						  En cas de déterioration de l'émetteur RCB3000, cette solution permet un dépannage rapide et éfficace, sans devoir changer l'entièreté de la face de votre modèle RCB3000.
+						</p>
+					</section>
+					<hr>                
 					<section id="blog-2013-12">
 						<h3>RCB3000 bidirectionelle</h3>
 						<img class="img-polaroid blog" src="../img/custom-product/2013-12-3000.jpg" alt="RCB3000 personnalisée avec retour d'informations">
