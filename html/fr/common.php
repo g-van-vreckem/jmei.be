0f468bd97aaf2abbc280457b77b2cf0facf6ec6d
g-van-vreckem <gvanvreckem@gmail.com>
2014-09-26 10:48:46 +0200

    missing keywords bug correction

--- html/fr/common.php
+++ html/fr/common.php
@@ -7,6 +7,7 @@
 	$ogLocale = 'en_US';
 	$title = 'JMei Solution-Electronic-Radio transmission - Radiocommande industrielle';
 	$description = 'JMei est spécialisé dans la conception et la production de Radiocommandes industrielles sur mesure. Nous assurons un service de qualité et une production rapide.';
+	$keywords = '';
 	$pageFooterScripts = '';
 	$ext = '.php'; //correct URL depending on the URL rewrite rules
 	DEFINE( 'SHOW_CERTIFICATION', false);
