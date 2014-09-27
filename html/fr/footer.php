18014de869d1e90bb74d8e0b48d42e9a191dd0af
g-van-vreckem <gvanvreckem@gmail.com>
2014-09-27 11:53:58 +0200

    Upgrade Google Analytics from ga to analytics
    Google changed their analytics code.
    Upgrade your account if it wasn't done automaticaly, then go live.

--- html/fr/footer.php
+++ html/fr/footer.php
@@ -19,11 +19,15 @@
         <script src="../js/plugins.js"></script>
 <?php echo $pageFooterScripts; ?>
         <script src="../js/main.js"></script>
-        <script>
-            var _gaq=[['_setAccount','UA-37918376-1'],['_gat._anonymizeIp'],['_trackPageview']];
-            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)}(document,'script'));
-        </script>
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-37918376-1', 'auto');
+			ga('set', 'anonymizeIp', true);
+			ga('send', 'pageview');
+		</script>
     </body>
 </html>
