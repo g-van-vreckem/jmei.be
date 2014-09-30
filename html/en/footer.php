10ef7b59c2bc2fef695efe2214547edab81f59f9
g-van-vreckem <gvanvreckem@gmail.com>
2014-09-30 22:59:59 +0200

    space/tab

--- html/en/footer.php
+++ html/en/footer.php
@@ -13,10 +13,10 @@
 			</div>
 		</div>
 		<script src="../js/vendor/jquery-1.9.1.min.js"></script>
-        <script src="../js/vendor/bootstrap.min.js"></script>
-        <script src="../js/plugins.js"></script>
+		<script src="../js/vendor/bootstrap.min.js"></script>
+		<script src="../js/plugins.js"></script>
 <?php echo $pageFooterScripts; ?>
-        <script src="../js/main.js"></script>
+		<script src="../js/main.js"></script>
 		<script>
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
