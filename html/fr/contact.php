e25bf8a9628b6677c3479d65a27cbbfa084af929
g-van-vreckem <gvanvreckem@gmail.com>
2014-09-30 23:24:39 +0200

    changed map lang to fr space/tab

--- html/fr/contact.php
+++ html/fr/contact.php
@@ -6,7 +6,7 @@
 
 	$pageId = 'contact';
 	$pageFooterScripts = <<<PAGESC
-		<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&amp;sensor=false&amp;language=en"></script> <!-- Translator Task: Adapt the &language= -->
+		<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&amp;sensor=false&amp;language=fr"></script> <!-- Translator Task: Adapt the &language= -->
 
 PAGESC;
 	include 'header.php';
@@ -26,7 +26,7 @@ PAGESC;
 						<label>Message <span>(Requis)</label>
 						<textarea id="message" title="Please type your message!" name="message" rows="12" cols="72" class="required span12"></textarea>
 						<button id="submit-btn" type="submit" class="btn">Envoi</button>
-						<input type="hidden" name="lang" value="en">
+						<input type="hidden" name="lang" value="fr">
 						<input type="hidden" name="reason" value="Contact">
 					 </fieldset>
 				</form>	
@@ -55,11 +55,12 @@ PAGESC;
 					TVA: BE0 534 889 177<br>
 					<a href="pdf/JMei%20sprl.pdf">Coordonnées bancaires</a></p>
 				</div>
-            </div>
-            <div class="row">
-                <div class="span12">
-	                <div id="map-canvas"></div>
-					<small><a target="_blank" href="https://maps.google.be/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=123+rue+de+rochefort,+Marloie&amp;aq=&amp;sll=50.154465,4.624975&amp;sspn=3.720241,5.751343&amp;t=h&amp;ie=UTF8&amp;hq=&amp;hnear=Chauss%C3%A9e+de+Rochefort+123,+6900+Marche-en-Famenne,+Luxembourg,+R%C3%A9gion+wallonne&amp;z=14&amp;ll=50.199664,5.317268" style="color:#0000FF;text-align:left">Agrandir la carte</a></small>
+			</div>
+			<div class="row">
+				<div class="span12">
+					<div id="map-canvas"></div>
+					<small><a target="_blank" href="https://maps.google.be/maps?f=q&amp;source=embed&amp;hl=fr&amp;geocode=&amp;q=123+rue+de+rochefort,+Marloie&amp;aq=&amp;sll=50.154465,4.624975&amp;sspn=3.720241,5.751343&amp;t=h&amp;ie=UTF8&amp;hq=&amp;hnear=Chauss%C3%A9e+de+Rochefort+123,+6900+Marche-en-Famenne,+Luxembourg,+R%C3%A9gion+wallonne&amp;z=14&amp;ll=50.199664,5.317268" style="color:#0000FF;text-align:left">Agrandir la carte</a></small>
 				</div>
 			</div>
+
  <?php include 'footer.php';?>
