8809b05c96b4b6b0c430a8248f46c9b2f372c1bd
JMei-Manu <manu.piraprez@jmei.be>
2014-10-06 00:27:14 +0200

    rcb700 fr - clean code (1/2) - from rcb 90 fr

--- html/fr/produit-RCB700.php
+++ html/fr/produit-RCB700.php
@@ -1,40 +1,38 @@
 <?php
 	include 'common.php';
-	$title = 'JMei - RCB700 - Commande radio';
+	$title = 'JMei - RCB700 - Commande radio forestière';
 	$description = 'Radiocommande ultra-compacte, rigidité accrue de part son boîtier en aluminium, cette commande à distance prendra facilement place dans vos applications forestières et agricoles';
 	$keywords = 'RCB700,commande radio,radiocommande ultra-compacte,applications agricoles,usages industriels,homme-mort,safeball,applications industrielles et forestières';
-
 	$pageId = 'RCB700';
 	$swiper = new Medias('/img/product-RCB700/swiper/');
 	include 'header.php';
 	include 'nav.php';
-
 ?>
 			<div class="row">
 <?php include 'product-nav.php'; ?>
 				<div class="span9">
 					<a name="highlight"></a>
-					<h3>Radiocommande de type RCB700</h3>
+					<h3>Radiocommande RCB700</h3>
 					<div class="row">
 <?php echo $swiper->html; ?>                        
 					</div>
 					<p></p>
 					<p><a href="<?php echo Site::relativeUrl('RCB700.pdf');?>" class="btn btn-success btn-large"><i class="icon-white icon-arrow-down"></i> Télécharger le PDF</a></p>
-					<a name="features"></a><h4>Caractéristiques
-					</h4>
+					<p><a href="<?php echo Site::relativeUrl('KEY');?>">Consulter la fiche technique</a></p>                      
+					<a name="features"></a>
+					<h4>Description</h4>
 					<ul>
-					  <li>Boîtier émetteur ultra compact, léger, très robuste, réalisé en fonte d&rsquo;aluminium, structure renforcée pour usages industriels, boîtier très ergonomique, adapté spécialement aux environnements agressifs. </li>
+						<li>Boîtier émetteur ultra compact, léger, très robuste, réalisé en fonte d&rsquo;aluminium, structure renforcée pour usages industriels, boîtier très ergonomique, adapté spécialement aux environnements agressifs. </li>
 						<li>Clé de mémoire disponible, pour une interchangeabilité facile des émetteurs.</li>
 						<li>Les pictogrammes et les voyants LED de l'émetteur sont placés selon les exigences du client et permettent ainsi une excellente visualisation des commandes.<br>
 					    En mode bidirectionnel, les informations provenant du récepteur sont affichées sur voyants LED ou afficheur 7 segments. Cet émetteur offre beaucoup de possibilités de personnalisations.</li>
 						<li>Convient pour tous types d'usages industriels nécessitant des fonctions TOR (1 vitesse), boîtier &quot;homme-mort&quot;, safeball,...</li>
 					</ul>
 					<a name="specifications"></a>
-					<h4>Spécifications</h4>
+					<h4>Avantages</h4>
 					<ul>
-						
 						<li>Gamme de fréquences:  434 MHz (64 canaux) ou 868MHz.</li>
-                        <li>Adresses et fréquences paramétrables par les boutons de l'émetteur. Aucune intervention nécessaire sur le récepteur.</li>
+						<li>Adresses et fréquences paramétrables par les boutons de l'émetteur. Aucune intervention nécessaire sur le récepteur.</li>
                         <li>Puissance d'émission: < 10 mW</li>
                         <li>Modulation: FM : FSK</li>
                         <li>Portée de la radiocommande: 200m</li>
@@ -45,139 +43,42 @@
                         <li>Possibilité d'alimentation fixe en 12 ou 24V DC (en option).</li>
                         <li>Possibilité d'options: <a href="<?php echo Site::relativeUrl('KEY');?>">clé mémoire</a>, <a href="<?php echo Site::relativeUrl('TCA27');?>">démarrage sécurisé</a>, <a href="<?php echo Site::relativeUrl('TANDEM');?>">coordination d'équipements</a>.</li>
                         <li>Récepteur RECB JMei, possibilité de commandes analogiques, digitales, série (RS232 ou RS485) ou bus de terrain (CAN, Profibus, Ethernet,...).</li>						</ul>
-					<a name="uses"></a><h4>Applications</h4>
-					<p>Très performante dans toutes les applications industrielles et forestières, sa fabrication en aluminium lui permet une solidité incomparable.</p>
-					<img src="/img/product-RCB700/rcb700_2.jpg" alt="Applications forestières" />
-<?php if ( SHOW_CERTIFICATION ) : ?>
-					<a name="certification"></a><h4>Certification</h4>
-					<ul>
-						<li>EMC: EN61000-6-2 et EN61000-6-4.</li>
-                        <li>Safety: EN 60950.</li>
-                        <li>Spectrum (R&amp;TTE Directive 1999/5/EC): EN300-220.</li>
-					</ul>
-<?php endif ?>
-					<hr>
-					<h4>Gamme</h4>
+       <hr>									
+                  <a name="accessories"></a>
+					<h4>Exemple de réalisation</h4>                       
 					<div class="row">
 						<div class="span3">
-							<h5>RCB700 Arrêt d'urgence</h5>
-							<img src="/img/product-RCB700/rcb700_3.jpg" alt="Arrêt d'urgence"> 
-							<h6>Fonctions diponibles :							</h6>
-							<ul>
-								<li>Un ou deux  boutons-poussoirs à impulsions  (commande TOR).</li>
-								<li>Un bouton-poussoir arrêt d&rsquo;urgence, coup-de-poing.</li>
-								<li>Retour d&rsquo;informations sous forme de témoins LED (en option).</li>
-							</ul>
-							<p><strong>Applications:</strong> commande de treuil, machine de carrière (concasseur, crible, bande transporteuses), boitier homme-mort pour la sécurisation de la maintenance de machine, commande de tous type de machines simples.</p>
+							<h6>RCB700 avec scanner intégré</h6>
+ 
+                    
+					<a name="features"></a>                            
+							<img src="../img/product-RCB700/rcb700_bot1.jpg" alt="Télécommande avec scanner">
+					<p><a href="<?php echo Site::relativeUrl('SCAN.pdf');?>">Consulter la fiche technique</a></p>                              
 						</div>
 						<div class="span3">
-							<h5>RCB700 BP TOR</h5>
-							<img src="/img/product-RCB700/rcb700_4.jpg" alt="Commande tout-ou-rien"> 
-							<h6>Fonctions diponibles:</h6>
-							<ul>
-							  <li>Jusqu'à 8 boutons-poussoirs à impulsions (commande TOR).</li>
-								<li>Un bouton-poussoir arrêt d&rsquo;urgence, coup-de-poing (en option).</li>
-								<li>Retour d&rsquo;informations sous forme de témoins LED (en option).</li>
-							</ul>
-							<p><strong>Applications:</strong> commande de treuil, machines de levage, machines agricoles ou forestières, équipements de carrière (concasseur, crible, bande transporteuses), radiocommande pour la sécurisation de la maintenance de machine, radiocommande embarquée à bord d&rsquo;engins mobiles de chantier.</p>
+							<h6>Radio personnalisée (couleur du pont roulant)</h6>
+							<img src="../img/product-RCB90/rcb90_bot4.jpg" alt="Télécommande pour pont roulant personnalisée">
 						</div>
 						<div class="span3">
-							<h5>RCB700 4 digits</h5>
-							<img src="/img/product-RCB700/rcb700_5.jpg" alt="Afficheur de télécommande"> 
-						  <h6>Fonctions diponibles:</h6>
-							<ul>
-							  <li>Jusqu'à 6 boutons-poussoirs à impulsions (commande TOR).</li>
-								<li>Un bouton-poussoir arrêt d&rsquo;urgence, coup-de-poing (en option).</li>
-								<li>Retour d&rsquo;informations sous forme d&rsquo;affichage (en option). Permet de transférer des messages ou tous types de mesures comme par exemple poids, débit, vitesse, distance, température ou autre information. Affichage de 4 digits 7 segments, système très lumineux.</li>
-							</ul>
-							<p><strong>Applications:</strong> contrôle de tout type de machine avec retour d&rsquo;information, machines de levage, machines agricoles ou forestières, équipements de sidérurgie ou de fonderie, radiocommande pour la sécurisation de la maintenance de machine, radiocommande embarquée à bord d&rsquo;engins mobiles de chantier.</p>
-						</div>
+							<h6>RCB90/M - Boîte à boutons 14 fonctions</h6>
+							<img src="../img/product-RCB90/rcb90_bot3.jpg" alt="Radiocommande industrielle">
+						</div>                                                
 					</div>
 					<div class="row">
 						<div class="span3">
-							<h5>RCB700 Forest</h5>
-							<img src="/img/product-RCB700/rcb700_6.jpg" alt="Radio forestière"> 
-							<h6>Fonctions diponibles:</h6>
-							<ul>
-							  <li>Un bouton-poussoir marche/arrêt.</li>
-								<li>Un bouton-poussoir arrêt d&rsquo;urgence, coup-de-poing.</li>
-								<li>Un ou 2 boutons, 3 positions (enrouler/dérouler).</li>
-							  <li>Un bouton 3 postions (accélérateur+/-)</li>
-								<li>Livré avec ensemble de commande machine: 1 vérin, 1 mécanisme de commande de pédale de gaz et arrêt du tracteur.<br>
-							  </li>
-							</ul>
-							<p><strong>Applications: </strong>machines forestières, treuillage avec variation hydraulique, commande de tous types de machines de génie civil avec asservissement hydraulique.</p>
+							<h6>Arrêt d'urgence haute qualité</h6>
+							<img src="../img/product-RCB90/rcb90_bot5.jpg" alt="Arrêt d'urgence télécommande">
 						</div>
 						<div class="span3">
-							<h5>RCB700 FULL</h5>
-							<img src="/img/product-RCB700/rcb700_7.jpg" alt="Commande à distance robuste"> 
-							<h6>Fonctions diponibles:</h6>
-							<ul>
-							  <li>12 boutons-poussoirs à impulsions (commande TOR).</li>
-								<li>Un bouton-poussoir arrêt d&rsquo;urgence, coup-de-poing (en option).</li>
-								<li>Retour d&rsquo;informations sous forme d&rsquo;affichage (en option). Permet de transférer des messages ou tous types de mesures comme par exemple poids, débit, vitesse, distance, température ou autre information.</li>
-							</ul>
-							<p><strong>Applications:</strong> contrôle de tous types de machines avec ou sans retour d&rsquo;information, machines de levage, machines de génie civil,  machines agricoles ou forestières, équipements de sidérurgie ou de fonderie, radiocommande pour la sécurisation de la maintenance de machine, radiocommande embarquée à bord d&rsquo;engins mobiles de chantier.</p>
-						</div>
+							<h6>Radiocommande indistrielle universelle</h6>
+							<img src="../img/product-RCB90/rcb90_bot2.jpg" alt="Boîte à boutons industrielle">
+						</div>						
 						<div class="span3">
-							<h5>RCB700 SCAN</h5>
-                          <img src="/img/product-RCB700/rcb700_8.jpg?v=2" alt="Commande à distance robuste">
-                          <h6>Fonctions diponibles:</h6>
-                          <ul>
-                              <li>Lecteur code-barres intégré (code 39/128 ou QR code).</li>
-                              <li>Retour d'informations sur LCD display ou TFT graphique.</li>
-                              <li>Commandes TOR et validation du SCAN.</li>
-                          </ul>
-                            <p><strong>Applications:</strong>Télécommande avec lecteur de code-barres intégré.<br>
-Ce système de commande permet d'accroître la sécurité du personnel (l'obligation de scan avant l'utilisation de la radiocommande permet la prise en considération de la présence de l'opérateur à un endroit connu du process).<br>
-Radiocommande utile également dans les systèmes de logistiques complexes. </p>
-						<p><a href="<?php echo Site::relativeUrl('SCAN.pdf');?>" class="btn btn-success btn-large"><i class="icon-white icon-arrow-down"></i> Télécharger le PDF</a></p>
-						</div>
-					</div>
-					<div class="row">
-						<div class="span3">
-							<h5>&nbsp;</h5>
-						</div>
-					</div>
-					<hr>
-					<a name="accessories"></a><h4>Accessoires</h4>
-					<div class="row">
-						<div class="span3">
-							<h6>Système de fixation par clips très pratique:</h6>
-							<img src="/img/product-RCB700/rcb700_10.jpg" alt="Clips de fixation pour télécommande">
-						  <p>Fixation à la ceinture, à la poche, ...</p>
-						</div>
-						<div class="span3">
-							<h6>Chargeur à microprocesseur:</h6>
-                            <img src="/img/product-RCB700/rcb700_11.jpg" alt="Chargeur pour radiocommande">
-                            <p>Assure la recharge en toute sécurité des accumulateurs. Temps de charge 4H.<br>
-                              Support mural. </p>
-						</div>
-						<div class="span3">
-						  <h6>Autres accessoires possibles:</h6>
-						  <ul>
-						    <li><strong>Housse de protection, harnais de portage</strong>. <br>
-						      <br>
-						    </li>
-						    <li><strong>Clef d&rsquo;identification</strong> <br>
-						      Associe l&rsquo;émetteur au récepteur. Permet une interchangeabilité rapide des émetteurs. <a href="<?php echo Site::relativeUrl('KEY');?>">Plus d'infos...</a><br>
-						      <br>
-						    </li>
-						    <li><strong>Démarrage sécurisé TCA27</strong>&nbsp;: Ce système permet le démarrage sécurisé de tous engins. Une séquence lumineuse aléatoire émise par le récepteur doit être encodée à l&rsquo;émetteur par l&rsquo;opérateur. L&rsquo;opérateur doit se trouver dans la zone de visibilité de la balise <a href="<?php echo Site::relativeUrl('TCA27');?>">Plus d'infos...</a></li>
-					      </ul>
-                      </div>
-					</div>
-					<div class="row">
-						<div class="span3">
-							<h6>&nbsp;</h6>
-						</div>
-						<div class="span3">
-							<h6>&nbsp;</h6>
-						</div>
-						<div class="span3">
-							<h6>&nbsp;</h6>
-						</div>
-					</div>
+							<h6>Clé mémoire adresse télécommande</h6>
+							<img src="../img/product-RCB90/rcb90_bot6.jpg" alt="Clé mémoire adresse télécommande">
+						</div>                        
+					</div>                  
+                    
 				</div>
 			</div>
 <?php include 'footer.php';?>
