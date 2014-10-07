7354f21bf00a1637f422b60309f8b9e0badcd070
JMei-Manu <manu.piraprez@jmei.be>
2014-10-07 23:56:44 +0200

    rcb1000 FR update

--- html/fr/produit-RCB1000.php
+++ html/fr/produit-RCB1000.php
@@ -1,26 +1,25 @@
 <?php
 	include 'common.php';
-	$title = 'JMei - RCB1000 - Pupitre industriel';
+	$title = 'JMei - RCB1000 - Télécommande industrielle';
 	$description = 'Pupitre de commande industriel haut de gamme, entièrement parsonnalisable, pour des applications industrielles les plus rudes';
 	$keywords = 'Radiocommande,RCB1000,pupitre ventral,radio analogique,émetteur, robuste, radiocommande,radiocommandes,radio commande,radio commandes,ponts roulant,commande à joystick,équipements de génie civil,manutention,équipement de levage mobile,camion de remorquage,pompe à béton ,camions de mélange,pont roulant,grue,grue hydraulique mobile,application mobile de transport de voiture,toutes applications de manutention,commande à distance, presse,machine,commande de machines mobiles avec réglage du déplacement progressif';
-
 	$pageId = 'RCB1000';
 	$swiper = new Medias('/img/product-RCB1000/swiper/');	
 	include 'header.php';
 	include 'nav.php';
-
 ?>
  			<div class="row">
 <?php include 'product-nav.php'; ?>
 				<div class="span9">
 					<a name="highlight"></a>
-				  <h3>Radiocommande de type RCB1000</h3>
+					<h3>Radiocommande RCB1000</h3>
 					<div class="row">
 <?php echo $swiper->html; ?>
 					</div>
 					<p></p>
 					<p><a href="<?php echo Site::relativeUrl('RCB1000.pdf');?>" class="btn btn-success btn-large"><i class="icon-white icon-arrow-down"></i> Télécharger le PDF</a></p>
-					<a name="features"></a><h4>Caractéristiques</h4>
+					<p><a href="<?php echo Site::relativeUrl('KEY');?>">Consulter la fiche technique</a></p>                  <a name="features"></a>
+                  <h4>Description</h4>
 					<ul>
 					  <li>Boîtier émetteur conçu avec une grande surface d&rsquo;accès pour une utilisation confortable.</li>
 					  <li>Léger, ergonomique et très robuste, réalisé en plastique renforcé pour usages industriels. <br>
@@ -34,9 +33,9 @@
 						<li>Livrable pour de multiples applications industrielles qui nécessitent un appareil robuste et fiable comme par exemple le domaine du levage et de la manutention, les machines de génie civil telles que grues à tour, grues hydrauliques sur camion, dumpers, chargeurs sur pneus, brise-roches, compacteurs et elle est particulièrement bien étudiée pour toute application dans le domaine minier ou maritime.</li>
 					</ul>
 					<a name="specifications"></a>
-				  <h4>Spécifications				  </h4>
-				  <ul>
-				    <li>Gamme de fréquences:  434 MHz (64 canaux) ou 868MHz.</li>
+					<h4>Avantages</h4>
+					<ul>
+						<li>Gamme de fréquences:  434 MHz (64 canaux) ou 868MHz.</li>
                         <li>Adresses et fréquences paramétrables par les joysticks de l'émetteur. Aucune intervention nécessaire sur le récepteur.</li>
                         <li>Joystick à contacts ou analogiques (max 8 canaux).</li>
                         <li>Puissance d'émission: < 10 mW</li>
@@ -47,121 +46,65 @@
                         <li>Batterie NiMH à grande autonomie (&gt;20h), sans effet mémoire.</li>
                         <li>Chargeur à micro-contrôleur de conception JMei (temps de charge: 4h).</li>
                         <li>Possibilité d'alimentation fixe en 12 ou 24V DC (en option).</li>
-                        <li>Possibilité d'options: <a href="<?php echo Site::relativeUrl('KEY');?>">clé mémoire</a>, <a href="<?php echo Site::relativeUrl('TCA27');?>">démarrage sécurisé</a>, <a href="<?php echo Site::relativeUrl('TANDEM');?>">coordination d'équipements</a>.</li>
-                        <li>Récepteur RecB JMei, possibilité de commandes analogiques, digitales, série (RS232 ou RS485) ou bus de terrain (CANOpen, Profibus, Ethernet,...)</li>
+                        <li>Possibilité d'options: <a href="<?php echo Site::relativeUrl('KEY');?>">clé mémoire</a>, 
 					</ul>
-					<h6>&nbsp;</h6>
-
-					<a name="uses"></a><h4>Applications</h4>
-					<p>Outre les applications classiques telles que ponts roulants, portiques, grues, engins de levage en général, l&rsquo;émetteur RCB1000 est particulièrement indiqué pour les applications industrielles lourdes. <br>
-L&rsquo;émetteur RCB1000 est également indiqué lorsque l&rsquo;application nécessite de nombreuses fonctions et l&rsquo;affichage d&rsquo;informations complexes. <br>
-La grande face facilement aménageable permet un grand nombre de fonctions tout en conservant une bonne ergonomie. <br> 
-ex:
-<br>
-Navire de dragage marin pour Jan de Null /2008 <br>
-Plateforme de manutention Airbus/2008  Pont roulant d&rsquo;aciérie Arcelor Mittal Belval 2008.<br>
-<br>
-<img src="../img/product-RCB1000/rcb1000_2.jpg" alt="Applications industrielles" />
-				  <a name="receiver"></a></p>
-					<h4>&nbsp;</h4>					
-<?php if ( SHOW_CERTIFICATION ) : ?>
-					<a name="certification"></a>
-				  <h4>Certifications</h4>
+					<hr>
+					<a name="accessories"></a>
+                  <h4>Options</h4>
 					<ul>
-						
-						<li>EMC: EN61000-6-2 et EN61000-6-4.</li>
-                        <li>Safety: EN 60950.</li>
-                        <li>Spectrum (R&amp;TTE Directive 1999/5/EC): EN300-220.</li>
-					</ul>
-<?php endif ?>
+						<li>Transmission en mode bidirectionnel.</li>
+						<li>Led de diagnostique et retour d'information.</li>
+						<li>Afficheur graphic couleur.</li>
+						<li>Clé d'identification (remplacement rapide d'un émetteur endommagé).</li>
+						<li>Démarrage sécurisé TCA27.</li>
+						<p><a href="<?php echo Site::relativeUrl('KEY');?>">Plus d'infos...</a></p>
+					</ul>  
+					<a name="accessories"></a>
+					<h4>Accessoires</h4>
+					<ul>
+						<li>Housse de protection en cuir.</li>
+						<li>Chargeur à microprocesseur.</li>
+						<li>Accumulateur interchangeable NIMH.</li>
+						<p><a href="<?php echo Site::relativeUrl('KEY');?>">Plus d'infos...</a></p>
+					</ul>					
 					<hr>
-					<h4>Gamme</h4>
+					<a name="uses"></a>
+					<h4>Applications</h4>
+					<ul>
+						<li>Applications industrielles lourdes.</li>
+						<li>Applications demandant de nombreuses fonctions.</li>
+					</ul>                  
+                  <hr>
+                  <a name="accessories"></a>
+					<h4>Exemple de réalisation</h4>                       
 					<div class="row">
 						<div class="span3">
-							<h5>RCB1000</h5>
-							<img src="../img/product-RCB1000/rcb1000_3.jpg" alt="RB1000 pupitre industriel"> 
-							<h6>Fonctions diponibles:</h6>
-							<ul>
-							  <li>Jusqu'à 40 fonctions &quot;tout-ou-rien&quot; (boutons à bascule ou boutons-poussoirs).</li>
-								<li>1 bouton d'arrêt d'urgence, coup-de-poing.</li>
-								<li>3 joysticks multiaxes.</li>
-								<li>Clé mémoire en option, permet de permuter rapidement avec l&rsquo;émetteur de réserve.</li>
-								<li><strong>Toute forme de personnalisation possible.</strong></li>
-							</ul>
-							<p><strong>Applications:</strong> commande ponts roulants ou engins de levage complexes, commande à distance de machines pour mines et carrière, commande de toutes machines maritimes.</p>
+							<h6>Radiocommande - prise en main sécuritaire</h6>
+							<img src="../img/product-RCB1000/rcb1000_bot1.jpg" alt="Radiocommande avec prise en main sécuritaire">
 						</div>
 						<div class="span3">
-							<h5>RCB1000</h5>
-							<img src="../img/product-RCB1000/rcb1000_4.jpg" alt="Commande à distance industrielle"> 
-							<h6>Fonctions diponibles:</h6>
-							<ul>
-							  <li>Jusqu'à 40 fonctions &quot;tout-ou-rien&quot; (boutons à bascule ou boutons-poussoirs).</li>
-								<li>8 sticks de commande uniaxe.</li>
-								<li>2 ou 4 boutons rotatifs analogiques.</li>
-								<li>1 bouton d'arrêt d'urgence, coup-de-poing.</li>
-								<li>Clé mémoire en option, permet de permuter rapidement avec l&rsquo;émetteur de réserve.</li>
-								<li>Retour d&rsquo;informations sous forme de témoins LED.</li>
-								<li><strong>Toute forme de personnalisation possible.</strong></li>
-							</ul>
-							<p><strong>Applications:</strong> commande à distance de presses ou machines similaires, commande de machines mobiles avec réglage du déplacement progressif.</p>
+							<h6>Radiocommande - joysticks personnalisés</h6>
+							<img src="../img/product-RCB1000/rcb1000_bot2.jpg" alt="Radiocommande - joysticks personnalisés">
 						</div>
 						<div class="span3">
-							<h5>RCB1000</h5>
-							<p><img src="../img/product-RCB1000/rcb1000_5.jpg" alt="Télécommande à joysticks"> 
-</p>
-							<h6>Fonctions diponibles:</h6>
-							<ul>
-							  <li>2 joysticks multiaxes progressifs avec homme-mort (commande hydraulique).</li>
-								<li>2 ou 4 joysticks uniaxes pour commandes hydrauliques.</li>
-								<li>Jusqu'à 10 fonctions &quot;tout-ou-rien&quot; (boutons à bascule ou boutons-poussoirs).</li>
-								<li>1 bouton d'arrêt d'urgence, coup-de-poing.</li>
-								<li>Clé mémoire en option, permet de permuter rapidement avec l&rsquo;émetteur de réserve.</li>
-								<li>Retour d&rsquo;informations sous forme de témoins LED.</li>
-								<li><strong>Toute forme de personnalisation possible.</strong></li>
-							</ul>
-							<p><strong>Applications: </strong>commande tous types de machines de chantier ou d&rsquo;applications minières avec asservissement hydraulique, machines de forage, dumpers, bulldozers, groupes de concassage mobile, tunneliers, machines mobiles spéciales.</p>
-						</div>
+							<h6>Radiocommande industrielle analogique</h6>
+							<img src="../img/product-RCB1000/rcb1000_bot3.jpg" alt="Radiocommande industrielle analogique">
+						</div>                                                
 					</div>
 					<div class="row">
 						<div class="span3">
-							<h5>RCB1000</h5>
-							<img src="../img/product-RCB1000/rcb1000_6.jpg" alt="Boîtier de commande avec afficheur"> 
-							<h6>Fonctions diponibles:</h6>
-							<ul>
-							  <li>Jusqu'à 20 fonctions &quot;tout-ou-rien&quot; (boutons à bascule ou boutons-poussoirs).</li>
-								<li>2 boutons rotatifs avec potentiomètres analogiques.</li>
-								<li>2 sticks uniaxes progressifs.</li>
-								<li>Ecran d&rsquo;affichage LCD (caractères alphanumériques ou graphique).</li>
-								<li>1 bouton d'arrêt d'urgence, coup-de-poing.</li>
-								<li>Clé mémoire en option, permet de permuter rapidement avec l&rsquo;émetteur de réserve.</li>
-								<li>Retour d&rsquo;informations sous forme de témoins LED ou sur afficheur.</li>
-								<li><strong>Toute forme de personnalisation possible.</strong></li>
-							</ul>
-							<p><strong>Applications:</strong> tous types de machines mobiles demandant un retour d&rsquo;informations du poste de commande .</p>
+							<h6>Clé mémoire électronique pour radiocommande</h6>
+							<img src="../img/product-RCB1000/rcb1000_bot4.jpg" alt="Clé mémoire électronique pour radiocommande">
 						</div>
-						<div class="span6">
-						</div>
+						<div class="span3">
+							<h6>Radiocommande avec afficheur</h6>
+							<img src="../img/product-RCB1000/rcb1000_bot5.jpg" alt="Radiocommande avec afficheur">
+						</div>						
+						<div class="span3">
+							<h6>Démarrage sécurisé pour télécommande</h6>
+							<img src="../img/product-RCB1000/rcb1000_bot6.jpg" alt="Démarrage sécurisé pour télécommande">
+						</div>                        
 					</div>
-					<hr>
-					<a name="accessories"></a><h4>Accessoires</h4>
-					<div class="row">
-						<div class="span3">
-							<h6>Housse de protection en cuir:</h6>
-							<img src="../img/product-RCB1000/rcb1000_7.jpg" alt="Housse de protection télécommande">
-						  <p>Complète avantageusement la sangle de portage et protège efficacement l&rsquo;émetteur des chocs.</p>
-						</div>
-						<div class="span3">
-							<h6>Clé d'identification hardware:</h6>
-							<img src="../img/product-RCB1000/rcb1000_9.jpg" alt="Commande de machines">
-						  <p>Associe l&rsquo;émetteur au récepteur. Permet une interchangeabilité rapide des émetteurs.</p>
-							<p><a href="<?php echo Site::relativeUrl('KEY');?>">Plus d'infos...</a></p>
-						</div>
-						<div class="span3">
-							<h6>Démarrage sécurisé TCA27:</h6>
-							<img src="../img/product-RCB1000/rcb1000_10.jpg" alt="Système de sécurité JMei">
-						  <p>Ce système breveté permet le démarrage sécurisé de tout type d&rsquo;engins. Une séquence lumineuse aléatoire de 2 couleurs  émise par le récepteur doit être encodée à l&rsquo;émetteur par l&rsquo;opérateur.</p>
-							<p><a href="<?php echo Site::relativeUrl('TCA27');?>">Plus d'infos...</a></p>
-						</div>
 					</div>
 				</div>
 <?php include 'footer.php';?>
