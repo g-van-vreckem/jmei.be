e4a91ed39ff0a69e6cfb220fb2348fecbc5a1914
JMei-Manu <manu.piraprez@jmei.be>
2014-10-16 17:15:04 +0200

    RCB700 - Helice PDF FR update

--- html/common.php
+++ html/common.php
@@ -93,8 +93,8 @@
 			self::addPage ( new Page ( 'RCB90.pdf', 'fr', 'RCB90', '', Page::PDF, true, .6 ) );
 			self::addPage ( new Page ( 'RCB90-datasheet.pdf', 'fr', 'RCB90-datasheet', '', Page::PDF, true, .6 ) );
 			self::addPage ( new Page ( 'RCB700.pdf', 'en', 'JMei RCB700', '', Page::PDF, true, .6 ) );
-			self::addPage ( new Page ( 'RCB700.pdf', 'fr', 'JMei RCB700', '', Page::PDF, true, .6 ) );
-			self::addPage ( new Page ( 'Helice.pdf', 'fr', 'JMei Helice', '', Page::PDF, true, .6 ) );
+			self::addPage ( new Page ( 'RCB700.pdf', 'fr', 'RCB700', '', Page::PDF, true, .6 ) );
+			self::addPage ( new Page ( 'Helice.pdf', 'fr', 'Helice', '', Page::PDF, true, .6 ) );
 			self::addPage ( new Page ( 'Helice-datasheet.pdf', 'fr', 'Helice-datasheet', '', Page::PDF, true, .6 ) );
 			self::addPage ( new Page ( 'RCB1000.pdf', 'en', 'JMei RCB1000', '', Page::PDF, true, .6 ) );
 			self::addPage ( new Page ( 'RCB1000.pdf', 'fr', 'JMei RCB1000', '', Page::PDF, true, .6 ) );
