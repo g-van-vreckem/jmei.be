08beeae6014801bf1b83ee5541bc2e7678d18a26
JMei-Manu <manu.piraprez@jmei.be>
2014-10-22 23:52:01 +0200

    RCB1000 fr change texte

--- html/fr/produit-RCB1000.php
+++ html/fr/produit-RCB1000.php
@@ -2,7 +2,7 @@
 	include 'common.php';
 	$title = 'JMei - RCB1000 - Télécommande industrielle robuste - Radiocommande avec joystick';
 	$description = 'Pupitre de commande industriel haut de gamme, entièrement parsonnalisable, pour des applications industrielles les plus rudes';
-	$keywords = 'Radiocommande,RCB1000,pupitre ventral,radio analogique,émetteur, robuste, radiocommande,radiocommandes,radio commande,radio commandes,ponts roulant,commande à joystick,équipements de génie civil,manutention,équipement de levage mobile,camion de remorquage,pompe à béton ,camions de mélange,pont roulant,grue,grue hydraulique mobile,application mobile de transport de voiture,toutes applications de manutention,commande à distance, presse,machine,commande de machines mobiles avec réglage du déplacement progressif';
+	$keywords = 'JMei,RCB1000,Télécommande,Télécommande industrielle robuste,Radiocommande,Radiocommande avec joystick,radiocommande sécuritaire,télécommande de haute performance,pupitre ventral,télécommande sans fil,Boîtier ultra compact,Arrêt d\'urgence coup-de-poing,manipulateurs 2 axes,manipulateurs uniaxes,Sélecteurs à rappel,Sélecteurs à positions fixes,Fonctions analogiques,inclinomètre,Joystiques avec fonction homme-mort,LED de diagnostic,retour d\'information,Pont roulant,Marine,Camion hydraulique,Engins spéciaux,Transmission bidirectionnel,Afficheur,Clé d\'identification,Système Tandem maître/esclave,Démarrage sécurisé TCA27,Profibus,Ethernet,Canbus,Profinet,bus de terrain,Housse de protection en cuir,Chargeur à microprocesseur,Accumulateur NiMH</li>';
 	$pageId = 'RCB1000';
 	$swiper = new Medias('/img/product-RCB1000/swiper/');	
 	include 'header.php';
@@ -18,62 +18,59 @@
 					</div>
 					<p></p>
 					<p><a href="<?php echo Site::relativeUrl('RCB1000.pdf');?>" class="btn btn-success btn-large"><i class="icon-white icon-arrow-down"></i> Télécharger le PDF</a></p>
-
+					<p><a href="<?php echo Site::relativeUrl('RCB1000-datasheet.pdf');?>">Consulter la fiche technique</a></p>  
                   <a id="description"></a>
                   <h4>Description</h4>
-					<ul>
-					  <li>Boîtier émetteur conçu avec une grande surface d&rsquo;accès pour une utilisation confortable.</li>
-					  <li>Léger, ergonomique et très robuste, réalisé en plastique renforcé pour usages industriels. <br>
-				      </li>
-						<li>Arceau souple conçu pour absorber les chocs, permet une grande facilité d&rsquo;utilisation et protège très efficacement les manipulateurs et boutons de commandes (joysticks) contre toute fausse manoeuvre accidentelle.</li>
-						<li>Très facile à personnaliser grâce à sa grande surface disponible.</li>
-						<li>Les pictogrammes du pupitre émetteur sont gravés et adaptés aux besoins du client et permettent une excellente visualisation des commandes, de plus un rétroéclairage permet un emploi aisé dans des zones peu ou pas éclairées, comme de nuit et ceci afin d&rsquo;améliorer une fois de plus la sécurité d&rsquo;emploi.</li>
-						<li>Protection: IP65.</li>
-						<li>Alimentation : par batterie dont l&rsquo;autonomie est supérieure à 20h, mais le système peut être aussi installé en fixe et sera alimenté alors en 12 ou 24 V.</li>
-						<li>Protection par housse de protection en cuir industriel, prévu avec harnais de portage reposant sur les épaules, étudié pour un meilleur confort d&rsquo;utilisation.</li>
-						<li>Livrable pour de multiples applications industrielles qui nécessitent un appareil robuste et fiable comme par exemple le domaine du levage et de la manutention, les machines de génie civil telles que grues à tour, grues hydrauliques sur camion, dumpers, chargeurs sur pneus, brise-roches, compacteurs et elle est particulièrement bien étudiée pour toute application dans le domaine minier ou maritime.</li>
-					</ul>
+				  <p>Radiocommande extrêmement robuste et sécuritaire de haute performance de type pupitre ventral conçu pour les commandes de tout engin industriel dans les environnements lourds.</p>
+						<p>La RCB1000 est une télécommande sans fil configurable à souhait et de grande qualité. <br>Son design et les possibilités infinies de configuration autorisent toutes les possibilités de commandes de machines.</p>
+						<p>Boîtier ultra compact, très robuste, réalisé en plastique renforcé pour un usage industriel, boîtier très ergonomique, adapté spécialement aux environnements agressifs.</p>
+						<p>Arrêt d'urgence coup-de-poing, garantit une sécurité maximale.</p>
 					<a id="avantages"></a>
 					<h4>Avantages</h4>
 					<ul>
-						<li>Gamme de fréquences:  434 MHz (64 canaux) ou 868MHz.</li>
-                        <li>Adresses et fréquences paramétrables par les joysticks de l'émetteur. Aucune intervention nécessaire sur le récepteur.</li>
-                        <li>Joystick à contacts ou analogiques (max 8 canaux).</li>
-                        <li>Puissance d'émission: < 10 mW</li>
-                        <li>Modulation: FM : FSK</li>
-                        <li>Portée de la radiocommande: 200m</li>
-                        <li>Codage de la transmission: adresses 32 bits et CRC 16 bits.</li>
-                        <li>Option bidirectionnelle sur 1 seule fréquence (retour d'infos sur LCD).</li>
-                        <li>Batterie NiMH à grande autonomie (&gt;20h), sans effet mémoire.</li>
-                        <li>Chargeur à micro-contrôleur de conception JMei (temps de charge: 4h).</li>
-                        <li>Possibilité d'alimentation fixe en 12 ou 24V DC (en option).</li>
-                        <li>Possibilité d'options: <a href="<?php echo Site::relativeUrl('KEY');?>">clé mémoire</a>, 
+						<li>2 ou 3 ou 4 manipulateurs 2 axes.</li>
+						<li>Jusque 8 manipulateurs uniaxes.</li>
+						<li>Sélecteurs à rappel ou positions fixes.</li>
+						<li>Bouton-poussoir standard ou avec témoins lumineux.</li>
+						<li>Fonctions TOR jusqu'à 6 vitesses par joystick.</li>
+						<li>Fonctions analogiques proportionnelles.</li>
+						<li>Face et inscription personnalisables.</li>
+						<li>Fonction inclinomètre en option.</li>
+						<li>Joystiques avec fonction homme-mort.</li>
+						<li>Arrêt d'urgence coup-de-poing qualité militaire.</li>
+						<li>Accumulateur interchangeable NiMH.</li>
+						<li>LED de diagnostic et retour d'information en option.</li>
+						<li>Ecran LCD graphique en option.</li>
+						<li>Système exclusif de changements d'adresse, de fréquence, configuration par les boutons permettant l'interchangeabilité rapide et sécurisante des émetteurs (un seul émetteur de réserve pour  plusieurs équipements).</li>
 					</ul>
 					<hr>
 					<a id="options"></a>
                   <h4>Options</h4>
 					<ul>
 						<li>Transmission en mode bidirectionnel.</li>
-						<li>Led de diagnostique et retour d'information.</li>
-						<li>Afficheur graphic couleur.</li>
-						<li>Clé d'identification (remplacement rapide d'un émetteur endommagé).</li>
+						<li>Afficheur.</li>
+						<li>Clé d'identification (remplacement rapide d'un émetteur endommagé).</li>  
+						<li>Système Tandem maître/esclave.</li>
 						<li>Démarrage sécurisé TCA27.</li>
+						<li>Récepteur type RECB ou RCB7000, possibilité de connexion Profibus, Ethernet, Canbus, Profinet ou autre bus de terrain.</li>
 						<p><a href="<?php echo Site::relativeUrl('OPTION');?>">Plus d'infos...</a></p>
 					</ul>  
 					<a id="accessoires"></a>
 					<h4>Accessoires</h4>
 					<ul>
 						<li>Housse de protection en cuir.</li>
-						<li>Chargeur à microprocesseur.</li>
-						<li>Accumulateur interchangeable NIMH.</li>
+						<li>Chargeur à microprocesseur de conception JMei.</li>
+						<li>Accumulateur interchangeable NiMH.</li>
 						<p><a href="<?php echo Site::relativeUrl('ACCESSORY');?>">Plus d'infos...</a></p>
 					</ul>					
 					<hr>
 					<a id="applications"></a>
 					<h4>Applications</h4>
 					<ul>
-						<li>Applications industrielles lourdes.</li>
-						<li>Applications demandant de nombreuses fonctions.</li>
+						<li>Pont roulant.</li>
+						<li>Marine.</li>
+						<li>Camion hydraulique.</li>
+						<li>Engins spéciaux, …</li>
 					</ul>                  
                   <hr>
                   <a id="réalisations"></a>
