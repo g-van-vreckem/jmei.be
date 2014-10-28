c3474492b1cc96a88a42cdfc2e111c1656e6fe60
JMei-Manu <manu.piraprez@jmei.be>
2014-10-28 17:47:54 +0100

    Sadamec add page FR

---
+++ html/fr/radio-sadamec.php
@@ -0,0 +1,36 @@
+<?php
+	include 'common.php';
+	$title = 'Service et pièces pour télécommande Sadamec';
+	$description = 'Forts d’une expérience de 25 ans dans la conception et la fabrication de radiocommandes, et seul fabricant en Belgique, la société JMei propose des solutions sur-mesure pour tous ses clients';
+	$keywords = 'JMei,radiocommandes industrielles,Belgium,fabrication de radiocommandes,fabricant en Belgique,solutions sur-mesure,société IRC,idées innovantes,équipe dynamique,radiocommande industrielle,produits de haute qualité,environnements industriels,service rapide et personnalisé,matériaux de haute qualité,prix attractifs,leaders européens,normes de sécurité,gamme de radiocommandes,Boîtes à boutons double vitesse,Pupitres à commandes progressives,Retour d\'informations,Options sécuritaires,Bus de terrain,profibus,can bus,ethernet,profinet,Mines et carrières,Maritime,Forêts et équipements agricoles,Transport routier,Bâtiments et Travaux Publics,BTP,Eolien et énergies renouvelables,transmissions radioélectriques,solutions personnalisées';
+
+	$pageId = 'about';
+	include 'header.php';
+	include 'nav.php';
+
+?>
+ 					<div class="row">
+						<div class="span3">
+							<img src="../img/sadamec/jmei.jpg" alt="Télécommande SADAMEC">
+						</div>
+						<div class="span3">
+							<img src="../img/sadamec/fr.jpg" alt="Télécommande SADAMEC">
+						</div>
+						<div class="span3">
+							<img src="../img/sadamec/pok.jpg" alt="Télécommande SADAMEC">
+						</div>                                                
+					</div>  
+					<hr>                             
+			<h4>Un service SADamec radiocommande assuré</h4>
+				<p>Il y a quelques mois, POK- fabricant de matériel de lutte contre l'incendie nous annonçait que dans le cadre de son département Lance Monitor Radiocommandée et ensuite dans son nouveau département POK Technology, pouvoir réaliser les études, le développement et la réalisation de commandes à distance sans fil ou filaire pour le secteur de la sécurité incendie <u>et pour l'industrie</u>.<br>Cette démarche était renforcée par l'arrivée du directeur technique de SADAMEC.</p>
+					<hr> 
+			<h4>Les télécommandes SADamec confiées à la société JMei</h4>
+				<p>Aujourd'hui, POK décide de se centrer sur son activité principale liée au secteur de l'incendie et cède à la société <strong>JMei</strong> la partie radio commande du secteur industriel.<br>C'est donc<strong> JMei</strong> qui pourra vous aider pour entretenir, dépanner et mettre à niveau ou tout simplement vous offrir des pièces de rechange pour l'ensemble du matériel de <strong>télécommande SADAMEC</strong>.</p>
+					<hr> 
+			<h4>POK et JMei redonnent naissance aux services SADamec</h4>
+				<p><strong>JMei</strong> est au cœur du métier de la radiocommande depuis 30 ans par les sociétés Collard-IRC et JMei. Elle est basée en Belgique francophone (Ardenne belge) et impliquée sur le marché français.<br>Elle se fera un plaisir de vous satisfaire dans des délais courts et d'analyser avec vous vos besoins en radiocommande.<br>JMei fabrique vos radios sur-mesure en respectant les normes sécuritaires imposées dans votre secteur.<br>
+<strong>POK et JMei</strong> sont certains de par cette décision de mieux répondre aux demandes du secteur industriel.</p>
+
+			<p>N'hésitez pas à <a href="<?php echo Site::relativeUrl('contact');?>">nous contacter</a>, JMei sera toujours en mesure de vous proposer la meilleure solution!</p>
+
+ <?php include 'footer.php';?>
