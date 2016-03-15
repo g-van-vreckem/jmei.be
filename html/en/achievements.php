9890e92ce91cfed4ca8596fb5a2bb17ceeeb3a82
JMei-Manu <manu.piraprez@jmei.be>
2016-03-15 22:59:11 +0100

    Réalisation EN

--- html/en/achievements.php
+++ html/en/achievements.php
@@ -17,17 +17,17 @@
 							<li class="nav-header">CUSTOMISED RADIO REMOTE CONTROLS</li>
 							<li class="active"><a href="#blog-2015-10">Technologie Infra-Rouge <i class="icon-chevron-right pull-right"></i></a></li> 
                          <li><a href="#blog-2015-05">Télécommande maritime <i class="icon-chevron-right pull-right"></i></a></li> 
-                         <li><a href="#blog-2015-02">Radio-télécommande double vitesse <i class="icon-chevron-right pull-right"></i></a></li> 
-                         <li><a href="#blog-2014-11">Télécommande forestière <i class="icon-chevron-right pull-right"></i></a></li> 
-                         <li><a href="#blog-2014-10">Télécommande Safeball <i class="icon-chevron-right pull-right"></i></a></li>                               
-                         <li><a href="#blog-2014-09">Télécommande avec répéteur radio <i class="icon-chevron-right pull-right"></i></a></li>                         
-                         <li><a href="#blog-2014-06">Radiocommande pour locotracteur <i class="icon-chevron-right pull-right"></i></a></li>
-                         <li><a href="#blog-2014-05">Radio scan code-barres <i class="icon-chevron-right pull-right"></i></a></li>
-                         <li><a href="#blog-2014-04">Radiocommande pour bande transporteuse <i class="icon-chevron-right pull-right"></i></a></li>
-                         <li><a href="#blog-2014-03">Télécommande ultra-compacte <i class="icon-chevron-right pull-right"></i></a></li>
-							<li><a href="#blog-2014-02">Commande proportionnelle CANbus <i class="icon-chevron-right pull-right"></i></a></li>                         <li><a href="#blog-2014-01">Arceau de protection <i class="icon-chevron-right pull-right"></i></a></li>
-							<li><a href="#blog-2013-12">Télécommande avec afficheurs <i class="icon-chevron-right pull-right"></i></a></li>
-							<li><a href="#blog-2013-11">Commande de machine agricole <i class="icon-chevron-right pull-right"></i></a></li>
+                         <li><a href="#blog-2015-02">Double-speed radio remote control<i class="icon-chevron-right pull-right"></i></a></li> 
+                         <li><a href="#blog-2014-11">Forestry remote control<i class="icon-chevron-right pull-right"></i></a></li> 
+                         <li><a href="#blog-2014-10">Safeball remote control<i class="icon-chevron-right pull-right"></i></a></li>                               
+                         <li><a href="#blog-2014-09">Remote control with radio repeater<i class="icon-chevron-right pull-right"></i></a></li>                         
+                         <li><a href="#blog-2014-06">Radio remote control for shunters<i class="icon-chevron-right pull-right"></i></a></li>
+                         <li><a href="#blog-2014-05">Radio barcode scanner<i class="icon-chevron-right pull-right"></i></a></li>
+                         <li><a href="#blog-2014-04">Radio remote control for conveyor belt<i class="icon-chevron-right pull-right"></i></a></li>
+                         <li><a href="#blog-2014-03">Ultracompact remote control<i class="icon-chevron-right pull-right"></i></a></li>
+							<li><a href="#blog-2014-02">CANbus proportional control<i class="icon-chevron-right pull-right"></i></a></li>                         <li><a href="#blog-2014-01">Protective guard<i class="icon-chevron-right pull-right"></i></a></li>
+							<li><a href="#blog-2013-12">Remote control with displays<i class="icon-chevron-right pull-right"></i></a></li>
+							<li><a href="#blog-2013-11">Agricultural machine control<i class="icon-chevron-right pull-right"></i></a></li>
 						</ul>
 					</div>
 				</div>
@@ -52,114 +52,113 @@
 					<hr> 
 					<a class="anchor" id="blog-2015-02"></a>
 					<section>
-						<h3>Radio-télécommande double vitesse - JMei S-LYT</h3>
-						<img class="img-polaroid blog" src="../img/custom-product/2015-02-slyt.jpg?v=1" alt="télécommande pour le levage">
-					  <p>JMei S-LYT.<br>Radio-télécommande industrielle ultra-légère adaptée aux applications de levage.<br>Cette commande à distance "plug and play" permet de nombreuses configurations (3e vitesse, double palan, fonction Tandem, maître-esclave, pitch and catch: envois du pont roulant depuis un émetteur et réception via un autre,...).</p>
+						<h3>Double speed radio remote control - JMei S-LYT</h3>
+						<img class="img-polaroid blog" src="../img/custom-product/2015-02-slyt.jpg?v=1" alt="Double speed radio remote control">
+					  <p>JMei S-LYT.<br>Ultralight industrial radio remote control suitable for lifting applications.<br>This "plug and play" remote control allows numerous configurations (third gear, double hoist, tandem function, master-slave, pitch and catch: sending a travelling crane from one transmitter and reception using another, etc.).</p>
 					</section>
 					<hr> 
 					<a class="anchor" id="blog-2014-11"></a>
 					<section>
-						<h3>Commande forestière - Télécommande pour treuil</h3>
-						<img class="img-polaroid blog" src="../img/custom-product/2014-11-700.jpg?v=1" alt="télécommande forestière">
-					  <p>Commande à distance pour treuil forestier.<br>Cette télécommande sans fil forestière et agricole en aluminium allie légèreté et robustesse.<br>Hautement sécuritaire avec son arrêt d'urgence de qualité militaire, elle permet la commande de deux treuils indépendants.<br>Les fonctions supplémentaires permettent de commander l'allumage du moteur à distance, de régler le régime moteur et d'activer un avertisseur sonore (klaxon).</p>
+						<h3>Forestry control – Remote control for a winch</h3>
+						<img class="img-polaroid blog" src="../img/custom-product/2014-11-700.jpg?v=1" alt="Remote control for a winch">
+					  <p>Remote control for forestry winch.<br>This forestry and agricultural wireless remote control made of aluminium combines lightness and robustness.<br>Highly secure with its military-quality emergency stop, it enables two winches to be controlled independently.<br>Additional functions enable the engine to be turned on remotely, the engine speed to be adjusted and an audible warning (horn) to be activated.</p>
 					</section>
 					<hr> 
 					<a class="anchor" id="blog-2014-10"></a>
 					<section>
-						<h3>Télécommande Safeball - Radiocommande "Homme-mort"</h3>
-						<img class="img-polaroid blog" src="../img/custom-product/2014-10-700.jpg?v=1" alt="télécommande homme-mort">
-						<p>Radio-télécommande avec système safeball.<br>
-						La télécommande de sécurité  safeball comporte une boule ergonomique à sécurité redondante (deux boutons-poussoirs). <br>
-						Cette commande bimanuelle trouve facilement sa place dans les systèmes &quot;homme-mort&quot; de sécurité.<br> 
-						Le système radiocommandé sera actif au moment où les deux contacts de la safeball seront actionnés.<br>
-						Ce système peut être couplé avec tous les modèles de télécommande RC de la gamme JMei.
+						<h3>Safeball remote control – "Dead man" radio remote control</h3>
+						<img class="img-polaroid blog" src="../img/custom-product/2014-10-700.jpg?v=1" alt="Dead man radio remote control">
+						<p>Radio remote control with Safeball system.<br>
+						The Safeball safety remote control includes an ergonomic redundant safety ball (two push buttons). <br>
+						This two-handed control easily finds its place among "dead man" safety systems.<br> 
+						The radio remote control system will be active when the two Safeball switches are activated.<br>
+						This system can be coupled with all the radio-controlled remote control models from the JMei range.
 						</p>
 					</section>
 					<hr> 
 					<a class="anchor" id="blog-2014-09"></a>
 					<section>
-						<h3>Télécommande avec répéteur radio (et réseau d'antennes)</h3>
-						<img class="img-polaroid blog" src="../img/custom-product/2014-09-700.jpg?v=1" alt="Répéteur pour radiocommandes">
-						<p><strong>Couverture réseau.</strong><br>
-						La couverture réseau permet la commande à distance d'applications depuis un endroit très contraignant du site (au niveau de la transmission radio: silos à grains, cuveries, caves, tunnels,..).<br>
-						Cette solution est réalisée par le placement d’antennes (actives ou passives) reliées entre elles par  un bus de terrain.<br>
-						Les commandes radio sont ainsi possibles en tout endroit de l’usine, sans provoquer d'interruption de la transmission HF.<br>
-						Un module répéteur additionnel permet des échanges radio depuis les endroits les plus confinés.<br>
+						<h3>Remote control with radio repeater (and antenna network)</h3>
+						<img class="img-polaroid blog" src="../img/custom-product/2014-09-700.jpg?v=1" alt="Remote control with radio repeater">
+						<p><strong>Network coverage.</strong><br>
+						The network coverage enables the remote control of applications from a very restricted area on the site (in terms of radio transmission: grain silos, wineries, sellers, tunnels, etc.).<br>
+						This solution is created by locating antennas (active or passive) connected to each other by a fieldbus.<br>
+						Radio control is therefore possible at any place in the factory, without causing an interruption to HF transmission.<br>
+						An additional repeater module enables radio exchanges from the most confined places.<br>
 						<br>
-						<strong>Module répéteur.</strong><br>
-						Ce module recopie intégralement les informations HF de manière transparente, permettant de doubler la distance de transmission.							<br>
-						Une alimentation sur batterie (en option) permet une utilisation et un positionnement modulables du répéteur.<br>
-						Son développement en mode multi-fréquences permet de conserver la vitesse initiale de transmission du système.<br>
-						Utilisation en mode uni/bidirectionnel.</p>
+						<strong>Repeater module.</strong><br>
+						This module copies all of the HF information transparently, enabling transmission distance to be doubled.							<br>
+						A battery power supply (optional) enables flexible use and positioning of the repeater.<br>
+						Its expansion in multi-frequency mode enables the initial transmission speed of the system to be retained.<br>
+						Use in uni-/bi-directional mode.</p>
 					</section>
 					<hr> 
 					<a class="anchor" id="blog-2014-06"></a>
 				  <section>
-						<h3>Commande à distance pour locotracteur (engins sur rails)</h3>
-						<img class="img-polaroid blog" src="../img/custom-product/2014-06-3000.jpg?v=1" alt="Commande à distance pour locotracteur">
-						  <p>Télécommande avec combinateur spécialement développé pour la commande à distance de locotracteur.
+						<h3>Remote control for shunter (machines on rails)</h3>
+						<img class="img-polaroid blog" src="../img/custom-product/2014-06-3000.jpg?v=1" alt="Remote control for shunter">
+						  <p>Remote control with combiner specially developed for a shunter remote control.
 							<br>
-						  Le manipulateur uniaxe est composé de positions fixes (système de freinage) et de positions à rappels (accélération).<br>
-						  Ajouté à la commande du locotracteur, une transmission d'inclinomètres permet la visualisation d'une inclinaison trop importante de l'émetteur (et donc de l'opérateur) en salle de contrôle déportée.</p>
+						  The uniaxial switch is made up of fixed positions (braking system) and springback positions (acceleration).<br>
+						  Added to the shunter control, an inclinometer transmission gives a display in the remote control room when the transmitter (and therefore the operator) is sloping at too great an angle.</p>
 					</section>
 					<hr>
 					<a  class="anchor" id="blog-2014-05"></a>
 					<section>
-						<h3>Commande à distance avec lecteur de code-barres</h3>
-						<img class="img-polaroid blog" src="../img/custom-product/2014-05-700.jpg" alt="Commande à distance avec lecteur code barre">
-							<p><a href="<?php echo Site::relativeUrl('SCAN.pdf');?>" class="btn btn-success btn-large"><i class="icon-white icon-arrow-down"></i> Télécharger le PDF</a></p>
-						  <p>Télécommande avec lecteur de code-barres intégré.<br>
-							Ce système de commande permet d'accroître la sécurité du personnel (l'obligation de scan avant l'utilisation de la radiocommande permet la prise en considération de la présence de l'opérateur à un endroit connu du process).<br>
-							Radiocommande utile également dans les systèmes de logistiques complexes.</p>
+						<h3>Remote control with barcode reader</h3>
+						<img class="img-polaroid blog" src="../img/custom-product/2014-05-700.jpg" alt="Remote control with barcode reader">
+							<p><a href="<?php echo Site::relativeUrl('SCAN.pdf');?>" class="btn btn-success btn-large"><i class="icon-white icon-arrow-down"></i>  Download PDF</a></p>
+						  <p>Remote control with built-in barcode reader.<br>
+							This control system means increased staff safety (the obligation to scan before using the radio remote control takes account when the operator is at a known place for the process).<br>
+							Radio remote control also useful in complex logistics systems.</p>
 					</section>
 					<hr>
 					<a class="anchor" id="blog-2014-04"></a>
                 	<section>
-						<h3>Commande à distance pour  bande transporteuse</h3>
-						<img class="img-polaroid blog" src="../img/custom-product/2014-04-helice.jpg" alt="Commande à distance avec afficheur">
-						  <p>Radiocommande multi-récepteurs.
+						<h3>Remote control for conveyor belts</h3>
+						<img class="img-polaroid blog" src="../img/custom-product/2014-04-helice.jpg" alt="Remote control for conveyor belts">
+						  <p>Multi-receiver radio remote control.
 							<br>
-						  Cette commande a distance a été réalisée pour la commande de 8 moteurs différents.<br>
-						  L'émetteur envoie des commandes sur 1 des 8 récepteurs,  sélectionné à l'aide d'un commutateur rotatif.<br>
-						La validation de la sélection est affichée sur afficheur 7 segments, via une communication bidirectionnelle afin d'accroître la sécurité.</p>
+						  This remote control has been produced for controlling 8 different motors.<br>
+						  The transmitter sends commands to 1 of the 8 receivers, selected using a rotary switch.<br>
+						Acceptance of the selection is displayed on a 7-segment display, using bi-directional communication in order to increase safety.</p>
 					</section>
 					<hr>
 					<a  class="anchor" id="blog-2014-03"></a>
                 	<section>
-						<h3>Télécommande pocket</h3>
-						<img class="img-polaroid blog" src="../img/custom-product/2014-03-pocket.jpg?v=1" alt="Télécommande ultra-compacte">
-						  <p>Radiocommande pocket industrielle.
+						<h3>Pocket remote control</h3>
+						<img class="img-polaroid blog" src="../img/custom-product/2014-03-pocket.jpg?v=1" alt="ultra-compact remote control">
+						  <p>Industrial pocket radio remote control.
 							<br>
-						  Cette télécommande ultra-compacte est destinée pour des commandes simples d'applications industrielles.<br>
-						  Son récepteur avec fonctions intégrées permet de pallier à la perte ou déterioration de l'émetteur.<br>
+						  This ultra-compact remote control is designed for simple control of industrial applications.<br>
+						  Its receiver unit with integrated functions compensates for the loss of or damage to the transmitter.<br>
 						  </p>
 					</section>
 					<hr> 
 					<a class="anchor" id="blog-2014-02"></a>
 					<section>
-						<h3>Télécommande analogique / CANopen personnalisée</h3>
-						<img class="img-polaroid blog" src="../img/custom-product/2014-02-3000.jpg" alt="Télécommande industrielle personnalisée">
-						  <p>Radiocommande proportionnelle CANbus (Canopen) pour machines mobiles.
+						<h3>Customised analogue/CANopen remote control</h3>
+						<img class="img-polaroid blog" src="../img/custom-product/2014-02-3000.jpg" alt="Customised remote control">
+						  <p>CANbus (CANopen) gradual radio remote control for mobile machines.
 							<br>
-						  Cet émetteur est destiné principalement à la commande de trois fonctions bien distinctes de l'application.<br>
-						  Afin d'accroître la sécurité lors des différentes manipulations, les couleurs présentes sur  la machine industrielle (différentes selon les mouvements) ont été 
-						  recopiées sur les actionneurs de l'émetteur de la télécommande.<br>
+						  This transmitter is mainly intended for controlling three very separate functions of the application.<br>
+						  In order to increase safety during various handling manoeuvres, the colours on the industrial machine (different depending on movements) have been copied onto the remote control transmitter switches.<br>
 						  </p>
 					</section>
 					<hr> 
 					<a class="anchor" id="blog-2014-01"></a>
 					<section>
-						<h3>Arceau de protection (SAV)</h3>
-						<img class="img-polaroid blog" src="../img/custom-product/2014-01-3000.jpg" alt="RCB3000 personnalisée avec retour d'informations">
-						<p>Nouvel arceau, souple et robuste à la fois, développé par JMei.<br>
-						  En cas de déterioration de l'émetteur RCB3000, cette solution permet un dépannage rapide et éfficace, sans devoir changer l'entièreté de la face de votre modèle RCB3000.
+						<h3>Protective guard (SAV)</h3>
+						<img class="img-polaroid blog" src="../img/custom-product/2014-01-3000.jpg" alt="Protective guard">
+						<p>New guard, both flexible and robust, developed by JMei.<br>
+						  In the event that the RCB3000 transmitter gets damaged, this solution enables rapid and effective troubleshooting, without having to change the entire front of your RCB3000 model.
 						</p>
 					</section>
 					<hr>                
 					<a class="anchor" id="blog-2013-12"></a>
 					<section>
 						<h3>Bi-directional RCB3000</h3>
-						<img class="img-polaroid blog" src="../img/custom-product/2013-12-3000.jpg" alt="RCB3000 personnalisée avec retour d'informations">
+						<img class="img-polaroid blog" src="../img/custom-product/2013-12-3000.jpg" alt="Bi-directional remote control">
 						<p>RCB3000 produced custom-built according to the customer's . <br>
 					    Model with feedback on 7-segment display units: displaying weight, temperature and pressure.<br>
 					    Confirmations of movements (coming from the receiver unit) are indicated through LED warning lights above each switch.
@@ -169,7 +168,7 @@
 					<a class="anchor" id="blog-2013-11"></a>
 					<section>
 						<h3>RCB700 - Warzee S.A. OEM Group </h3>
-						<img class="img-polaroid blog" src="../img/custom-product/2013-11-700.jpg" alt="RCB700 - Commande de machine agricole - Warzee">
+						<img class="img-polaroid blog" src="../img/custom-product/2013-11-700.jpg" alt="Remote control of a remote unwinder">
 						<p>Completely customised RCB700.<br>
 						  Remote control of a remote unwinder (on the fork of an agricultural machine) from the machine's cab using radio frequencies.
 						</p>
