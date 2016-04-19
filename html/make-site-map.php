130149c40cf5ca581eb9ffc3915248cefaa4af06
g-van-vreckem <gvanvreckem@gmail.com>
2016-04-19 19:17:20 +0200

    adapt sitemap generator

--- html/make-site-map.php
+++ html/make-site-map.php
@@ -3,31 +3,34 @@
 	date_default_timezone_set('Etc/GMT-1');
 	header('Cache-Control: no-cache, must-revalidate'); // HTTP/1.1
 	header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // Date in the past
-	$xml = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
-	/*$xml .= '<?xml-stylesheet type="text/xsl" href="gss.xsl"?>' . PHP_EOL; */
-	$xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL;
+	function makeSiteMap($domain, $filename) {
+		$xml = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
+		/*$xml .= '<?xml-stylesheet type="text/xsl" href="gss.xsl"?>' . PHP_EOL; */
+		$xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL;
 
-	foreach ( Site::$pages As $lang => $pages ) {
-		foreach ( $pages As $key => $value ) {
-			if ( $value->priority > 0 ) {
-				$f = $value->filename();
-				$d = new DateTime();
-				if ( file_exists ( $f ) ) {
-					$d->setTimeStamp(filemtime($f));
+		foreach ( Site::$pages As $lang => $pages ) {
+			foreach ( $pages As $key => $value ) {
+				if ( $value->priority > 0 ) {
+					$f = $value->filename();
+					$d = new DateTime();
+					if ( file_exists ( $f ) ) {
+						$d->setTimeStamp(filemtime($f));
+					}
+					$xml .=  '<url>';
+					$xml .=  '<loc>' . $value->url(Page::FULL) . '</loc>';
+					$xml .=  '<lastmod>' . $d->format(DateTime::W3C) . '</lastmod>';
+					$xml .=  '<priority>' . $value->priority . '</priority>';
+					$xml .=  '</url>' . PHP_EOL;
 				}
-				$xml .=  '<url>';
-				$xml .=  '<loc>' . $value->url(Page::FULL) . '</loc>';
-				$xml .=  '<lastmod>' . $d->format(DateTime::W3C) . '</lastmod>';
-				$xml .=  '<priority>' . $value->priority . '</priority>';
-				$xml .=  '</url>' . PHP_EOL;
 			}
 		}
+		$xml .=  '</urlset>'. PHP_EOL;
+		if (false === file_put_contents( __DIR__ . DIRECTORY_SEPARATOR . $filename, $xml)) {
+			echo "There was an error saving the {$filename} file";
+		} else {
+			echo "sitemap {$filename} generated<br>";
+		}
 	}
-	$xml .=  '</urlset>'. PHP_EOL;
-	if (false === file_put_contents( __DIR__ . DIRECTORY_SEPARATOR . 'sitemap.xml', $xml)) {
-		echo 'There was an error saving the sitemap.xml file';
-	} else {
-		header('Content-type: text/xml'); //sending xml data.
-		echo $xml;
-	}
-?>
+	makeSiteMap('http://www.jmei.be', 'sitemap.xml');
+	makeSiteMap('http://www.jmei.be', 'sitemap.default.php');
+	makeSiteMap('http://www.jmei.fr', 'sitemap.fourmies.php');
